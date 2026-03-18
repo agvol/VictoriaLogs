agvol/VictoriaLogs

commit 9599193918f4bc412f3e3875cea20b65b0995b68

metrics: add more merging metrics (#424)
Co-authored-by: Aliaksandr Valialkin <valyala@victoriametrics.com>

lib/logstorage/datadb.go
@@ -16,6 +16,7 @@ import (
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/fs"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
 	"github.com/VictoriaMetrics/VictoriaMetrics/lib/memory"
+	"github.com/VictoriaMetrics/metrics"
 )
 
 // The maximum size of big part.
@@ -54,14 +55,25 @@ type datadb struct {
 	// mergeIdx is used for generating unique directory names for parts
 	mergeIdx atomic.Uint64
 
-	inmemoryMergesTotal  atomic.Uint64
-	inmemoryActiveMerges atomic.Int64
+	inmemoryMergesTotal    atomic.Uint64
+	inmemoryActiveMerges   atomic.Int64
+	inmemoryMergeRowsTotal atomic.Uint64
 
-	smallPartMergesTotal  atomic.Uint64
-	smallPartActiveMerges atomic.Int64
+	smallPartMergesTotal    atomic.Uint64
+	smallPartActiveMerges   atomic.Int64
+	smallPartMergeRowsTotal atomic.Uint64
 
-	bigPartMergesTotal  atomic.Uint64
-	bigPartActiveMerges atomic.Int64
+	bigPartMergesTotal    atomic.Uint64
+	bigPartActiveMerges   atomic.Int64
+	bigPartMergeRowsTotal atomic.Uint64
+
+	// metrics that need to be updated directly
+	inmemoryPartMergeDuration *metrics.Summary
+	inmemoryPartMergeBytes    *metrics.Summary
+	smallPartMergeDuration    *metrics.Summary
+	smallPartMergeBytes       *metrics.Summary
+	bigPartMergeDuration      *metrics.Summary
+	bigPartMergeBytes         *metrics.Summary
 
 	// pt is the partition the datadb belongs to
 	pt *partition
@@ -184,10 +196,18 @@ func mustOpenDatadb(pt *partition, path string, flushInterval time.Duration) *da
 	ddb := &datadb{
 		pt:            pt,
 		flushInterval: flushInterval,
-		path:          path,
-		smallParts:    smallParts,
-		bigParts:      bigParts,
-		stopCh:        make(chan struct{}),
+
+		inmemoryPartMergeDuration: metrics.GetOrCreateSummary(`vl_merge_duration_seconds{type="storage/inmemory"}`),
+		inmemoryPartMergeBytes:    metrics.GetOrCreateSummary(`vl_merge_bytes{type="storage/inmemory"}`),
+		smallPartMergeDuration:    metrics.GetOrCreateSummary(`vl_merge_duration_seconds{type="storage/small"}`),
+		smallPartMergeBytes:       metrics.GetOrCreateSummary(`vl_merge_bytes{type="storage/small"}`),
+		bigPartMergeDuration:      metrics.GetOrCreateSummary(`vl_merge_duration_seconds{type="storage/big"}`),
+		bigPartMergeBytes:         metrics.GetOrCreateSummary(`vl_merge_bytes{type="storage/big"}`),
+
+		path:       path,
+		smallParts: smallParts,
+		bigParts:   bigParts,
+		stopCh:     make(chan struct{}),
 	}
 	ddb.rb.init(&ddb.wg, ddb.mustFlushLogRows)
 	ddb.mergeIdx.Store(uint64(time.Now().UnixNano()))
@@ -519,6 +539,7 @@ func (ddb *datadb) mustMergeParts(pws []*partWrapper, isFinal bool) {
 		mp.MustStoreToDisk(dstPartPath)
 		pwNew := ddb.openCreatedPart(&mp.ph, pws, nil, dstPartPath)
 		ddb.swapSrcWithDstParts(pws, pwNew, dstPartType)
+		ddb.updateMergeMetrics(dstPartType, mp.ph.RowsCount, startTime, mp.ph.CompressedSizeBytes)
 		return
 	}
 
@@ -588,6 +609,7 @@ func (ddb *datadb) mustMergeParts(pws []*partWrapper, isFinal bool) {
 	}
 
 	ddb.swapSrcWithDstParts(pws, pwNew, dstPartType)
+	ddb.updateMergeMetrics(dstPartType, srcRowsCount, startTime, dstSize)
 
 	d := time.Since(startTime)
 	if d <= time.Minute {
@@ -599,6 +621,23 @@ func (ddb *datadb) mustMergeParts(pws []*partWrapper, isFinal bool) {
 	rowsPerSec := int(float64(srcRowsCount) / durationSecs)
 	logger.Infof("merged (%d parts, %d rows, %d blocks, %d bytes) into (1 part, %d rows, %d blocks, %d bytes) in %.3f seconds at %d rows/sec to %q",
 		len(pws), srcRowsCount, srcBlocksCount, srcSize, dstRowsCount, dstBlocksCount, dstSize, durationSecs, rowsPerSec, dstPartPath)
+}
+
+func (ddb *datadb) updateMergeMetrics(partType partType, srcRowCount uint64, startTime time.Time, dstSize uint64) {
+	switch partType {
+	case partInmemory:
+		ddb.inmemoryMergeRowsTotal.Add(srcRowCount)
+		ddb.inmemoryPartMergeDuration.UpdateDuration(startTime)
+		ddb.inmemoryPartMergeBytes.Update(float64(dstSize))
+	case partSmall:
+		ddb.smallPartMergeRowsTotal.Add(srcRowCount)
+		ddb.smallPartMergeDuration.UpdateDuration(startTime)
+		ddb.smallPartMergeBytes.Update(float64(dstSize))
+	case partBig:
+		ddb.bigPartMergeRowsTotal.Add(srcRowCount)
+		ddb.bigPartMergeDuration.UpdateDuration(startTime)
+		ddb.bigPartMergeBytes.Update(float64(dstSize))
+	}
 }
 
 func (ddb *datadb) nextMergeIdx() uint64 {
@@ -669,6 +708,21 @@ type rowsBuffer struct {
 	nextIdx atomic.Uint64
 }
 
+func (rb *rowsBuffer) Len() uint64 {
+	shards := rb.shards
+	n := uint64(0)
+	for i := range shards {
+		shard := &shards[i]
+		shard.mu.Lock()
+		if shard.lr != nil {
+			n += uint64(shard.lr.Len())
+		}
+		shard.mu.Unlock()
+	}
+
+	return n
+}
+
 func (rb *rowsBuffer) init(wg *sync.WaitGroup, flushFunc func(lr *logRows)) {
 	shards := make([]rowsBufferShard, cgroup.AvailableCPUs())
 	for i := range shards {
@@ -680,7 +734,7 @@ func (rb *rowsBuffer) init(wg *sync.WaitGroup, flushFunc func(lr *logRows)) {
 }
 
 type rowsBufferShard struct {
-	wg        *sync.WaitGroup
+	wg        *sync.WaitGroup // wg is shared with datadb.
 	flushFunc func(lr *logRows)
 
 	mu         sync.Mutex
@@ -764,23 +818,35 @@ func (ddb *datadb) mustFlushLogRows(lr *logRows) {
 
 // DatadbStats contains various stats for datadb.
 type DatadbStats struct {
-	// InmemoryMergesTotal is the number of inmemory merges performed in the given datadb.
-	InmemoryMergesTotal uint64
+	// InmemoryMergesCount is the number of inmemory merges performed in the given datadb.
+	InmemoryMergesCount uint64
 
-	// InmemoryActiveMerges is the number of currently active inmemory merges performed by the given datadb.
-	InmemoryActiveMerges uint64
+	// ActiveInmemoryMerges is the number of currently active inmemory merges performed by the given datadb.
+	ActiveInmemoryMerges uint64
 
-	// SmallPartMergesTotal is the number of small file merges performed in the given datadb.
-	SmallPartMergesTotal uint64
+	// InmemoryRowsMerged is the number of rows merged to inmemory parts.
+	InmemoryRowsMerged uint64
 
-	// SmallPartActiveMerges is the number of currently active small file merges performed by the given datadb.
-	SmallPartActiveMerges uint64
+	// SmallMergesCount is the number of small file merges performed in the given datadb.
+	SmallMergesCount uint64
 
-	// BigPartMergesTotal is the number of big file merges performed in the given datadb.
-	BigPartMergesTotal uint64
+	// ActiveSmallMerges is the number of currently active small file merges performed by the given datadb.
+	ActiveSmallMerges uint64
 
-	// BigPartActiveMerges is the number of currently active big file merges performed by the given datadb.
-	BigPartActiveMerges uint64
+	// SmallRowsMerged is the number of rows merged to small parts.
+	SmallRowsMerged uint64
+
+	// BigMergesCount is the number of big file merges performed in the given datadb.
+	BigMergesCount uint64
+
+	// ActiveBigMerges is the number of currently active big file merges performed by the given datadb.
+	ActiveBigMerges uint64
+
+	// BigRowsMerged is the number of rows merged to big parts.
+	BigRowsMerged uint64
+
+	// PendingRows is the number of rows, which weren't flushed to searchable part yet.
+	PendingRows uint64
 
 	// InmemoryRowsCount is the number of rows, which weren't flushed to disk yet.
 	InmemoryRowsCount uint64
@@ -839,12 +905,17 @@ func (s *DatadbStats) RowsCount() uint64 {
 
 // updateStats updates s with ddb stats.
 func (ddb *datadb) updateStats(s *DatadbStats) {
-	s.InmemoryMergesTotal += ddb.inmemoryMergesTotal.Load()
-	s.InmemoryActiveMerges += uint64(ddb.inmemoryActiveMerges.Load())
-	s.SmallPartMergesTotal += ddb.smallPartMergesTotal.Load()
-	s.SmallPartActiveMerges += uint64(ddb.smallPartActiveMerges.Load())
-	s.BigPartMergesTotal += ddb.bigPartMergesTotal.Load()
-	s.BigPartActiveMerges += uint64(ddb.bigPartActiveMerges.Load())
+	s.InmemoryMergesCount += ddb.inmemoryMergesTotal.Load()
+	s.ActiveInmemoryMerges += uint64(ddb.inmemoryActiveMerges.Load())
+	s.InmemoryRowsMerged += ddb.inmemoryMergeRowsTotal.Load()
+	s.SmallMergesCount += ddb.smallPartMergesTotal.Load()
+	s.ActiveSmallMerges += uint64(ddb.smallPartActiveMerges.Load())
+	s.SmallRowsMerged += ddb.smallPartMergeRowsTotal.Load()
+	s.BigMergesCount += ddb.bigPartMergesTotal.Load()
+	s.ActiveBigMerges += uint64(ddb.bigPartActiveMerges.Load())
+	s.BigRowsMerged += ddb.bigPartMergeRowsTotal.Load()
+
+	s.PendingRows = ddb.rb.Len()
 
 	ddb.partsLock.Lock()
 

app/vlstorage/main.go
@@ -425,13 +425,23 @@ func writeStorageMetrics(w io.Writer, strg *logstorage.Storage) {
 	}
 	metrics.WriteGaugeUint64(w, fmt.Sprintf(`vl_storage_is_read_only{path=%q}`, *storageDataPath), isReadOnly)
 
-	metrics.WriteGaugeUint64(w, `vl_active_merges{type="storage/inmemory"}`, ss.InmemoryActiveMerges)
-	metrics.WriteGaugeUint64(w, `vl_active_merges{type="storage/small"}`, ss.SmallPartActiveMerges)
-	metrics.WriteGaugeUint64(w, `vl_active_merges{type="storage/big"}`, ss.BigPartActiveMerges)
+	metrics.WriteGaugeUint64(w, `vl_active_merges{type="storage/inmemory"}`, ss.ActiveInmemoryMerges)
+	metrics.WriteGaugeUint64(w, `vl_active_merges{type="storage/small"}`, ss.ActiveSmallMerges)
+	metrics.WriteGaugeUint64(w, `vl_active_merges{type="storage/big"}`, ss.ActiveBigMerges)
+	metrics.WriteGaugeUint64(w, `vl_active_merges{type="indexdb/inmemory"}`, ss.IndexdbActiveInmemoryMerges)
+	metrics.WriteGaugeUint64(w, `vl_active_merges{type="indexdb/file"}`, ss.IndexdbActiveFileMerges)
 
-	metrics.WriteCounterUint64(w, `vl_merges_total{type="storage/inmemory"}`, ss.InmemoryMergesTotal)
-	metrics.WriteCounterUint64(w, `vl_merges_total{type="storage/small"}`, ss.SmallPartMergesTotal)
-	metrics.WriteCounterUint64(w, `vl_merges_total{type="storage/big"}`, ss.BigPartMergesTotal)
+	metrics.WriteCounterUint64(w, `vl_merges_total{type="storage/inmemory"}`, ss.InmemoryMergesCount)
+	metrics.WriteCounterUint64(w, `vl_merges_total{type="storage/small"}`, ss.SmallMergesCount)
+	metrics.WriteCounterUint64(w, `vl_merges_total{type="storage/big"}`, ss.BigMergesCount)
+	metrics.WriteCounterUint64(w, `vl_merges_total{type="indexdb/inmemory"}`, ss.IndexdbInmemoryMergesCount)
+	metrics.WriteCounterUint64(w, `vl_merges_total{type="indexdb/file"}`, ss.IndexdbFileMergesCount)
+
+	metrics.WriteCounterUint64(w, `vl_rows_merged_total{type="storage/inmemory"}`, ss.InmemoryRowsMerged)
+	metrics.WriteCounterUint64(w, `vl_rows_merged_total{type="storage/small"}`, ss.SmallRowsMerged)
+	metrics.WriteCounterUint64(w, `vl_rows_merged_total{type="storage/big"}`, ss.BigRowsMerged)
+	metrics.WriteCounterUint64(w, `vl_rows_merged_total{type="indexdb/inmemory"}`, ss.IndexdbInmemoryItemsMerged)
+	metrics.WriteCounterUint64(w, `vl_rows_merged_total{type="indexdb/file"}`, ss.IndexdbFileItemsMerged)
 
 	metrics.WriteGaugeUint64(w, `vl_storage_rows{type="storage/inmemory"}`, ss.InmemoryRowsCount)
 	metrics.WriteGaugeUint64(w, `vl_storage_rows{type="storage/small"}`, ss.SmallPartRowsCount)
@@ -444,6 +454,9 @@ func writeStorageMetrics(w io.Writer, strg *logstorage.Storage) {
 	metrics.WriteGaugeUint64(w, `vl_storage_blocks{type="storage/inmemory"}`, ss.InmemoryBlocks)
 	metrics.WriteGaugeUint64(w, `vl_storage_blocks{type="storage/small"}`, ss.SmallPartBlocks)
 	metrics.WriteGaugeUint64(w, `vl_storage_blocks{type="storage/big"}`, ss.BigPartBlocks)
+
+	metrics.WriteGaugeUint64(w, `vl_pending_rows{type="storage"}`, ss.PendingRows)
+	metrics.WriteGaugeUint64(w, `vl_pending_rows{type="indexdb"}`, ss.IndexdbPendingItems)
 
 	metrics.WriteGaugeUint64(w, `vl_partitions`, ss.PartitionsCount)
 	metrics.WriteCounterUint64(w, `vl_streams_created_total`, ss.StreamsCreatedTotal)

lib/logstorage/indexdb.go
@@ -46,6 +46,27 @@ type IndexdbStats struct {
 
 	// IndexdbPartsCount is the number of parts in indexdb.
 	IndexdbPartsCount uint64
+
+	// IndexdbPendingItems is the number of pending items in IndexedDB before they are merged into the part.
+	IndexdbPendingItems uint64
+
+	// IndexdbActiveFileMerges is the number of active merges in indexdb.
+	IndexdbActiveFileMerges uint64
+
+	// IndexdbActiveInmemoryMerges is the number of active merges in indexdb.
+	IndexdbActiveInmemoryMerges uint64
+
+	// IndexdbFileMergesCount is the number of merges in indexdb.
+	IndexdbFileMergesCount uint64
+
+	// IndexdbInmemoryMergesCount is the number of merges in indexdb.
+	IndexdbInmemoryMergesCount uint64
+
+	// IndexdbFileItemsMerged is the number of items merged in indexdb.
+	IndexdbFileItemsMerged uint64
+
+	// IndexdbInmemoryItemsMerged is the number of items merged in indexdb.
+	IndexdbInmemoryItemsMerged uint64
 }
 
 type indexdb struct {
@@ -107,8 +128,15 @@ func (idb *indexdb) updateStats(d *IndexdbStats) {
 
 	d.IndexdbSizeBytes += tm.InmemorySizeBytes + tm.FileSizeBytes
 	d.IndexdbItemsCount += tm.InmemoryItemsCount + tm.FileItemsCount
+	d.IndexdbPendingItems += tm.PendingItems
 	d.IndexdbPartsCount += tm.InmemoryPartsCount + tm.FilePartsCount
 	d.IndexdbBlocksCount += tm.InmemoryBlocksCount + tm.FileBlocksCount
+	d.IndexdbActiveFileMerges = tm.ActiveFileMerges
+	d.IndexdbActiveInmemoryMerges = tm.ActiveInmemoryMerges
+	d.IndexdbFileMergesCount += tm.FileMergesCount
+	d.IndexdbInmemoryMergesCount += tm.InmemoryMergesCount
+	d.IndexdbFileItemsMerged += tm.FileItemsMerged
+	d.IndexdbInmemoryItemsMerged += tm.InmemoryItemsMerged
 }
 
 func (idb *indexdb) appendStreamTagsByStreamID(dst []byte, sid *streamID) []byte {

lib/logstorage/storage.go
@@ -19,17 +19,17 @@ import (
 
 // StorageStats represents stats for the storage. It may be obtained by calling Storage.UpdateStats().
 type StorageStats struct {
-	// RowsDroppedTooBigTimestamp is the number of rows dropped during data ingestion because their timestamp is smaller than the minimum allowed
+	// RowsDroppedTooBigTimestamp is the number of rows dropped during data ingestion because their timestamp is bigger than the maximum allowed.
 	RowsDroppedTooBigTimestamp uint64
 
-	// RowsDroppedTooSmallTimestamp is the number of rows dropped during data ingestion because their timestamp is bigger than the maximum allowed
+	// RowsDroppedTooSmallTimestamp is the number of rows dropped during data ingestion because their timestamp is smaller than the minimum allowed.
 	RowsDroppedTooSmallTimestamp uint64
 
-	// MaxDiskSpaceUsageBytes is the maximum disk space logs can use.
-	MaxDiskSpaceUsageBytes int64
-
-	// PartitionsCount is the number of partitions in the storage
+	// PartitionsCount is the number of partitions in the storage.
 	PartitionsCount uint64
+  
+  	// MaxDiskSpaceUsageBytes is the maximum disk space logs can use.
+	MaxDiskSpaceUsageBytes int64
 
 	// IsReadOnly indicates whether the storage is read-only.
 	IsReadOnly bool

commit 5730e6907b20be5b2ba18fc560d20b517511e88b

lib/logstorage: eliminate data race when reading inmemoryPart.ph after the inmemoryPart is returned to the pool inside datadb.swapSrcWithDstParts
The inmemoryPart is returned to the pool at the partWrapper.decRef() call before returning from datadb.swapSrcWithDstParts,
so it could be immediately re-used by concurrently running goroutine before returning from the datadb.swapSrcWithDstParts.
This can result in improper calculations of metrics for in-memory parts such as vl_rows_merged_total{type="storage/inmemory"}
and vl_merge_bytes{type="storage/inmemory"} .

The data race has been introduced in the commit 9599193918f4bc412f3e3875cea20b65b0995b68 .

lib/logstorage/datadb.go
@@ -539,9 +539,12 @@ func (ddb *datadb) mustMergePartsInternal(pws []*partWrapper, isFinal bool, drop
 		// Fast path: flush a single in-memory part to disk.
 		mp := pws[0].mp
 		mp.MustStoreToDisk(dstPartPath)
+		srcRowsCount := mp.ph.RowsCount
+		dstSize := mp.ph.CompressedSizeBytes
+
 		pwNew := ddb.openCreatedPart(&mp.ph, pws, nil, dstPartPath)
 		ddb.swapSrcWithDstParts(pws, pwNew, dstPartType)
-		ddb.updateMergeMetrics(dstPartType, mp.ph.RowsCount, startTime, mp.ph.CompressedSizeBytes)
+		ddb.updateMergeMetrics(dstPartType, srcRowsCount, startTime, dstSize)
 		return true
 	}
 
@@ -1355,8 +1358,7 @@ func mustRemoveUnusedDirs(path string, partNames []string) {
 	}
 }
 
-// appendPartsToMerge finds optimal parts to merge from src,
-// appends them to dst and returns the result.
+// appendPartsToMerge finds optimal parts to merge from src, appends them to dst and returns the result.
 func appendPartsToMerge(dst, src []*partWrapper, maxOutBytes uint64) []*partWrapper {
 	if len(src) < 2 {
 		// There is no need in merging zero or one part :)